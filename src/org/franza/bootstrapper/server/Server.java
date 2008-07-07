fde238c98dc1847734f15eb7cf330c5485372999
pfranza <pfranza@e927bb8c-2551-0410-abc1-032b8ed61618>
2008-07-07 21:46:40 +0000

    made server automatically scan for classes with main method and advertise them  made port specification optional, default 3000

--- src/org/franza/bootstrapper/server/Server.java
+++ src/org/franza/bootstrapper/server/Server.java
@@ -1,7 +1,13 @@
 package org.franza.bootstrapper.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import org.franza.bootstrapper.server.advertisementbeacon.Advertisement;
 import org.franza.bootstrapper.server.advertisementbeacon.Advertiser;
@@ -19,10 +25,11 @@ public class Server {
 	}
 
 	public static void main(final String[] args) throws NumberFormatException, IOException {
-
-		new Server(Integer.valueOf(args[0]));
-		final Advertiser ads = new Advertiser(Integer.valueOf(args[1]));
-		addAdvertisements(ads, Integer.valueOf(args[0]));
+		final Integer port = getPort();
+		System.out.println("Server started on port: " + port);
+		new Server(port);
+		final Advertiser ads = new Advertiser(port);
+		addAdvertisements(ads, port);
 		
 		while(true) {
 			try {
@@ -35,11 +42,83 @@ public class Server {
 	}
 
 	private static void addAdvertisements(final Advertiser ads, final int port) throws IOException {
+		for(final Class<?> c: findAllMainMethods()) {
+			ads.addAdvertisement(new Advertisement(c.getSimpleName(), c.getName(), InetAddress.getLocalHost().getHostAddress(), port));
+		}		
+	}
 
-			ads.addAdvertisement(new Advertisement("Name", "org.franza.bootstrapper.ClientServerTest", InetAddress.getLocalHost().getHostAddress(), port));
+	private static List<Class<?>> findAllMainMethods() {
+		final List<Class<?>> l = new ArrayList<Class<?>>();
+			
+		final String classPath = System.getProperty("java.class.path",".");	
+		final String[] cp = classPath.split(File.pathSeparator);
 
+		for (final String string : cp) {
+
+			final File f = new File(string);
+			if(f.isDirectory()) {
+				try {
+					for(final File e: f.listFiles()) {
+						searchDirectory(e, l, "");
+					}
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}				
+			} else if(f.isFile() && f.getName().toLowerCase().endsWith(".jar")) {
+				try {
+					searchJar(f, l);
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+		
+		return l;
+	}
+
+	private static void searchJar(final File f, final List<Class<?>> l) throws Exception {
+		final JarFile jar = new JarFile(f);
+		final Enumeration<JarEntry> en = jar.entries();
+		while(en.hasMoreElements()) {
+			final JarEntry e = en.nextElement();
+			if(e.getName().endsWith(".class") && !e.getName().contains("$")) {
+			
+				final Class<?> c = Class.forName(e.getName().substring(0, e.getName().length()-6).replace('/', '.'));
+				addItem(l, c);
+			}
+		}
+		
+	}
+
+	private static void addItem(final List<Class<?>> l, final Class<?> c) {
+		
+		if(c.getName().startsWith("junit."))
+			return;
+		
+		try {
+			c.getMethod("main", new Class[] {String[].class});
+			l.add(c);
+			System.out.println("   Advertising " + c);
+		} catch(final NoSuchMethodException nsme) {
+
+		}
+	}
+
+	private static void searchDirectory(final File f, final List<Class<?>> l, final String pack) throws Exception {
+		if(f.isFile() && f.getName().endsWith(".class") && !f.getName().contains("$")) {
+			final Class<?> c = Class.forName(pack + f.getName().substring(0, f.getName().length()-6));
+			addItem(l, c);
+		} else if(f.isDirectory()) {
+			for(final File e: f.listFiles()) {
+				searchDirectory(e, l, pack + f.getName() + ".");
+			}
+		}
 		
 	}
 	
+	public static int getPort() {
+		return Integer.valueOf(System.getProperty("port", "3000"));
+	}
 	
 }
